9f6160ccbe516126e00852ce93395060ce2b4c92
StijnPittomvils <pittomvils.stijn@gmail.com>
2018-12-05 11:34:36 +0100

    added select battle component, added enter playername component

--- frontEnd/lightGate/src/components/components.module.ts
+++ frontEnd/lightGate/src/components/components.module.ts
@@ -4,17 +4,23 @@ import { BattleComponent } from './battle/battle';
 import { JoinCreateComponent } from './join-create/join-create';
 import { CreationOptionsComponent } from './creation-options/creation-options';
 import { DeclareGuildNamesComponent } from './declare-guild-names/declare-guild-names';
+import { SelectBattleScreenComponent } from './select-battle-screen/select-battle-screen';
+import { EnterPlayernameComponent } from './enter-playername/enter-playername';
 @NgModule({
 	declarations: [LoginComponent,
     BattleComponent,
     JoinCreateComponent,
     CreationOptionsComponent,
-    DeclareGuildNamesComponent],
+    DeclareGuildNamesComponent,
+    SelectBattleScreenComponent,
+    EnterPlayernameComponent],
 	imports: [],
 	exports: [LoginComponent,
     BattleComponent,
     JoinCreateComponent,
     CreationOptionsComponent,
-    DeclareGuildNamesComponent]
+    DeclareGuildNamesComponent,
+    SelectBattleScreenComponent,
+    EnterPlayernameComponent]
 })
 export class ComponentsModule {}
