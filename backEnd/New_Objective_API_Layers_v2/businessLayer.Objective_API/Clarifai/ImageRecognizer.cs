58f086aec22311e8cdd3d02778e3ca24b9e31693
Denny Matthijs <denny.matthijs@student.ap.be>
2018-12-19 14:39:15 +0100

    Changed Location of Clarifai Classes

---
+++ backEnd/New_Objective_API_Layers_v2/businessLayer.Objective_API/Clarifai/ImageRecognizer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Clarifai.API;
+using Clarifai.DTOs.Inputs;
+
+namespace businessLayer.Objective_API.Clarifai
+{
+    public class ImageRecognizer
+    {
+        private ClarifaiClient client;
+        public ImageRecognizer()
+        {
+            this.client = new ClarifaiClient("44b3fcacff504de8b6c7ef46c753321b");
+        }
+
+        public async Task PredictLabels(string img = "https://static.standard.co.uk/s3fs-public/thumbnails/image/2016/10/19/13/city-bike-header2.jpg?width=1000&height=614&fit=bounds&format=pjpg&auto=webp&quality=70&crop=16:9,offset-y0.5")
+        {
+
+            var res = await this.client.PublicModels.GeneralModel
+            .Predict(new ClarifaiURLImage(img))
+            .ExecuteAsync();
+
+            foreach (var concept in res.Get().Data)
+            {
+                System.Diagnostics.Debug.WriteLine($"{concept.Name}: {concept.Value}");
+            }
+        }
+    }
+}
